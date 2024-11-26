73e89ec72ab879e8638eece3b7bc4e88891606d6
JARVIS <41569276+JARVIS-4356@users.noreply.github.com>
2024-11-26 23:16:03 +0530

    Build some Audio HALs/Audio Effect HALs & Fix build errors due to wrong filename & paths.

--- common.mk
+++ common.mk
@@ -41,8 +41,16 @@ PRODUCT_PACKAGES += \
     libaudioroute.vendor
 
 PRODUCT_PACKAGES += \
+    android.hardware.audio@2.0-impl \
+    android.hardware.audio@4.0-impl \
+    android.hardware.audio@5.0-impl \
     android.hardware.audio@6.0-impl \
+    android.hardware.audio@7.0-impl \
+    android.hardware.audio.effect@2.0-impl \
+    android.hardware.audio.effect@4.0-impl \
+    android.hardware.audio.effect@5.0-impl \
     android.hardware.audio.effect@6.0-impl \
+    android.hardware.audio.effect@7.0-impl \
     android.hardware.audio.service \
     android.hardware.soundtrigger@2.3-impl
 
